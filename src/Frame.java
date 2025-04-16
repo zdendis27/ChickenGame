a2f2cc347e1c56c34620a9849c415304ca4e524f
zNB <zdenekvacek007@gmail.com>
2025-04-16 10:40:05 +0200

    Zaklad JayFramu a vytvoreni okna.

---
+++ src/Frame.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class Frame extends JFrame {
+
+
+    ImageIcon icon = new ImageIcon("src/images/cartoon-strong-chicken-mascot-design-vector-29152871.jpg");
+
+    Frame(){
+        this.setVisible(true);
+        this.setSize(800, 600);
+        this.setResizable(false);
+        this.setTitle("CHICKEN GAME");
+        this.setIconImage(icon.getImage());
+        this.setDefaultCloseOperation(this.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+        this.getContentPane().setBackground(Color.CYAN);
+
+    }
+}
